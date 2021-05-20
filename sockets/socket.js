d311c1da00b330ea56188dfde7edd5cb60b90f3a
wlmc <williammcastro@msn.com>
2021-05-20 17:14:16 -0500

    terminado agregar una banda OK server

--- sockets/socket.js
+++ sockets/socket.js
@@ -15,20 +15,31 @@ bands.addBand( new Band('Metallica') );
 
 //Mensajes de sockets:
 io.on('connection', client => {//Escucha conexiones de los clientes y lo q sigue abajo lo ejecuta cuando alguien se conecta
-    console.log('Cliente conectado en el backend index.js');
+   console.log('Cliente conectado en el backend index.js');
 
-    client.emit('active-bands', bands.getBands() );
+   client.emit('active-bands', bands.getBands() );
 
-     //la siguiente linea esta escuchando el mensaje de votos de la banda, es decir desde flutter 'vote-band'
-     client.on('vote-band', ( payload ) => {
-      console.log('Escuchando el voto de la banda : ', payload );
+   //Votar por una banda:
+   //la siguiente linea esta escuchando el mensaje de votos de la banda, es decir desde flutter 'vote-band'
+   client.on('vote-band', ( payload ) => {
+      console.log('Escuchando el voto de la banda id : ', payload );
       bands.voteBand( payload.id );
       io.emit('active-bands', bands.getBands() ); //Emito a todos los conectados al server con io, en vez de client o socket.
-  });
+   });
+
+   //Anadir una banda:
+   client.on('add-band', ( payload ) => {
+      console.log('Agregando la banda : ', payload );
+      const newBand = new Band(payload.name);
+      bands.addBand( newBand );
+      io.emit('active-bands', bands.getBands() ); //Emito a todos los conectados al server con io, en vez de client o socket.
+   });
+
+   client.on
     
-    client.on('disconnect', () => { 
+   client.on('disconnect', () => { 
         console.log('Cliente desconectado en el backend index.js');
-     });
+   });
 
      // implementar event despues: client.on('event', data => { /* … */ });
 
