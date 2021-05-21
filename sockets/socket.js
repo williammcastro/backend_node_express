d3485504dd99e408a7d1fb3ddede9cda4b02200a
wlmc <williammcastro@msn.com>
2021-05-21 10:58:50 -0500

    bandNames Backend Server terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -10,7 +10,7 @@ bands.addBand( new Band('Heroes del Silencio') );
 bands.addBand( new Band('Metallica') );
 
 
-//console.log(bands); //ver las bandas
+//console.log(bands); //ver las bandas en consola
 
 
 //Mensajes de sockets:
@@ -35,29 +35,36 @@ io.on('connection', client => {//Escucha conexiones de los clientes y lo q sigue
       io.emit('active-bands', bands.getBands() ); //Emito a todos los conectados al server con io, en vez de client o socket.
    });
 
-   client.on
-    
+   //Borrar una banda:
+   client.on('delete-band', ( payload ) => {
+      console.log('id de la banda a borrar : ', payload );
+      bands.deleteBand( payload.id );
+      io.emit('active-bands', bands.getBands() ); //Emito a todos los conectados al server con io, en vez de client o socket.
+   });
+
+
+   //Escuchar cuando se desconecta un cliente:
    client.on('disconnect', () => { 
         console.log('Cliente desconectado en el backend index.js');
    });
 
      // implementar event despues: client.on('event', data => { /* … */ });
 
-     //Aqui se recibe el mensajeclave que envio el index.html
+     //Aqui se recibe el mensajeclave que envio el cliente desde index.html
      client.on('mensajeclave', ( payload ) => {
         console.log('recibido el mensaje clave en el backend index.js ;)', payload )
 
         //Ahora voy a responder cuando llegue el mensaje pero a todos los usuarios, pueden ser web o apps
         //puedo responder con emit(responder al cliente) o con io(todo el servidor)
-        io.emit('mensajeclaverespuestaatodos', {admin: 'nuevo mensaje para todos desde el backend index.js'});
+        io.emit('mensajeclaverespuestaatodos', {admin: 'nuevo mensaje para todos desde el backend socket.js'});
      });
 
+     //Mensaje desde la pag de status de flutter: un objeto con nombre y mensaje
      client.on('nuevo-mensaje', ( payload ) => {
-        console.log('recibido nuevo-mensaje...payload : ', payload)//emite a todos
+        console.log('recibido nuevo-mensaje desde pagina status de flutter...payload : ', payload)//emite a todos
         //io.emit('morral', payload);//emite a todos
         //client.broadcast.emit('morral', payload);//emite a todos menos al que lo emitio
         client.broadcast.emit('emitir-mensaje', payload);
-
-     })
+     });
 
 });
